922e36825818a661da61860c8d38ae18c9d06883
Evan Vogts <evanjvogts@gmail.com>
2019-10-28 13:54:10 -0500

    finished first drill

--- web-api-express/app.js
+++ web-api-express/app.js
@@ -11,31 +11,15 @@ app.listen(8000, () => {
   console.log('Express server is listening on port 8000!');
 });
 
-app.get('/burgers', (req, res) => {
-  res.send('We have juicy cheese burgers!');
-});
+app.get('/sum', (req, res) => {
+  const a = +req.query.a;
+  const b = +req.query.b;
 
-app.get('/pizza/pepperoni', (req, res) => {
-  res.send('Your pizza is on the way!');
-});
+  if(isNaN(a) || isNaN(b)) {
+    res.send ('both paramenters must be numbers');
+  } else {
+    const response =  `The sum of ${a} and ${b} is ${a+b}`;
+    res.send (response);
+  }
 
-app.get('/pizza/pineapple', (req, res) => {
-  res.send("We don't serve that here. Never call again!");
 });
-
-app.get('/echo', (req, res) => {
-  const responseText = `Here are some details of your request:
-    Base URL: ${req.baseUrl}
-    Host: ${req.hostname}
-    Path: ${req.path}
-    Fresh: ${req.fresh}
-  `;
-  res.send(responseText);
-});
-
-app.get('/queryViewer', (req, res) => {
-  console.log(req.query);
-  res.end(); //do not send any data back to the client
-});
-
-//
